655ab11e2bf361ed316c0b1a3330d267a75ef026
George K. Thiruvathukal <gkt@cs.luc.edu>
2021-06-05 16:22:00 -0500

    show how to calculate project day on commit timeline

--- git-commits-tree-diff.py
+++ git-commits-tree-diff.py
@@ -2,6 +2,9 @@ import os
 import os.path
 import sys
 import logging
+import dateutil.parser
+import datetime
+
 from functools import reduce
 
 import argparse
@@ -34,11 +37,12 @@ def go():
     git_checkout(args.branch)
 
     # Get list of commits from "first commit"
-    command = 'git log --reverse --pretty=format:"%H"'
+    command = 'git log --reverse --pretty=format:"%H;%ci"'
     total_loc = 0
     with os.popen(command) as git_log_pipe:
         # --root here allows you to use this instead of has for "beginning"
-        commits = ["--root"] + git_log_pipe.read().split()
+        commits = [ parse_commit_line(commit) for commit in git_log_pipe ]
+        #commits = git_log_pipe.read().split()
         commit_info_iter = process_commits(commits)
         delta_loc_iter = map(lambda info: info['delta_loc'], commit_info_iter)
         loc_sum = reduce(lambda x, y : x + y, delta_loc_iter, 0)
@@ -47,17 +51,28 @@ def go():
     logger.info("LOC = %d; KLOC = %.3f" % (loc_sum, kloc_sum))
     os.chdir(pwd)
 
+def parse_commit_line(commit_line):
+    (commit_hash, commit_raw_date) = commit_line.split(";")[:2]
+    parsed_date = dateutil.parser.parse(commit_raw_date)
+    return { 'hash' : commit_hash, 'date' : parsed_date }
 
-def process_commits(commits):
+def process_commits(git_commits):
+    # I know this is ugly but "root" does not have a hash and is a special case in git diff-tree
+    day0 = git_commits[0]['date']
+    commits = [{ 'hash' : '--root', 'date' : day0 }]  + git_commits
     logger = logging.getLogger("LOC:process_commits()") 
     loc_sum = 0
     for i in range(0, len(commits)-1):
-        g = git_diff_tree(commits[i], commits[i+1])
+        hashX = commits[i]['hash']
+        hashY = commits[i+1]['hash']
+        dateY = commits[i+1]['date']
+        g = git_diff_tree(hashX, hashY)
         loc_iter = map(lambda info: info['loc'], g)
         delta_sum = reduce(lambda x, y : x + y, loc_iter, 0)
         loc_sum += delta_sum
-        result = { 'hash' : commits[i], 'delta_loc' : delta_sum, 'loc_sum' : loc_sum }
-        logger.info("Hash: %(hash)s delta LOC=%(delta_loc)s cumulative LOC=%(loc_sum)s" % result)
+        commit_day = (dateY - day0).days
+        result = { 'hash' : hashY, 'delta_loc' : delta_sum, 'loc_sum' : loc_sum, 'day' : commit_day }
+        logger.info("Hash: %(hash)s Day: %(day)s;  delta LOC=%(delta_loc)s cumulative LOC=%(loc_sum)s" % result)
         yield result
 
 def git_checkout(hash):
