04d4381d17d2d16bf531ebf9e7e0f897e01e6099
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-07-17 16:35:23 +0000

    Remove logging

--- git-commits-tree-diff.py
+++ git-commits-tree-diff.py
@@ -1,12 +1,11 @@
 import json
-import logging
 import os
 
 from os.path import join, exists
 from functools import reduce
-from argparse import ArgumentError, ArgumentParser
+from argparse import ArgumentParser
 
-import dateutil.parser
+from dateutil.parser import parse as dateParse
 
 
 def get_argparse() -> ArgumentParser:
@@ -47,33 +46,31 @@ def repoExists(directory: str = ".") -> bool:
     return True
 
 
+def parseCommitLineFromLog(line: str) -> dict:
+    (hash, date) = line.split(";")[:2]
+    return {"hash": hash, "date": dateParse(date)}
+
+
 def go() -> bool:
     # Setup variables
     pwd = os.getcwd()
     args = get_argparse().parse_args()
     repoDirectory = args.directory
 
-    # Start logging
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger("LOC.go()")
-
     # Test if directory is a Git repo
     # If True, change directory to Git repo
     if repoExists(directory=repoDirectory) is False:
-        logger.error(f"Not a Git repo: {repoDirectory}")
         return False
     os.chdir(repoDirectory)
 
-    # Checkout specified branch
+    # Checkout specified branch in a subshell
     os.system(f"git checkout {args.branch}")
 
-    # Get list of commits from "first commit"
-    command = 'git log --reverse --pretty=format:"%H;%ci"'
-    total_loc = 0
-    with os.popen(command) as git_log_pipe:
-        # --root here allows you to use this instead of has for "beginning"
-        commits = [parse_commit_line(commit) for commit in git_log_pipe]
-        # commits = git_log_pipe.read().split()
+    # Get list of commits from starting from the first commit of the repository
+    # Git log help page: https://www.git-scm.com/docs/git-log
+    with os.popen(r'git log --reverse --pretty=format:"%H;%ci"') as gitLogPipe:
+        commits = [parseCommitLineFromLog(line=commit) for commit in gitLogPipe]
+
         commit_info_iter = process_commits(commits)
         commit_info = list(commit_info_iter)
         delta_loc_iter = map(lambda info: info["delta_loc"], commit_info)
@@ -83,8 +80,8 @@ def go() -> bool:
         write_json_file(args.save_json, commit_info)
 
     kloc_sum = loc_sum / 1000.0
-    logger.info("LOC = %d; KLOC = %.3f" % (loc_sum, kloc_sum))
     os.chdir(pwd)
+    return True
 
 
 def write_json_file(json_file, commit_info):
@@ -92,17 +89,10 @@ def write_json_file(json_file, commit_info):
         json.dump(commit_info, jsonf, indent=3)
 
 
-def parse_commit_line(commit_line):
-    (commit_hash, commit_raw_date) = commit_line.split(";")[:2]
-    parsed_date = dateutil.parser.parse(commit_raw_date)
-    return {"hash": commit_hash, "date": parsed_date}
-
-
 def process_commits(git_commits):
     # I know this is ugly but "root" does not have a hash and is a special case in git diff-tree
     day0 = git_commits[0]["date"]
     commits = [{"hash": "--root", "date": day0}] + git_commits
-    logger = logging.getLogger("LOC:process_commits()")
     loc_sum = 0
     for i in range(0, len(commits) - 1):
         hashX = commits[i]["hash"]
@@ -119,15 +109,10 @@ def process_commits(git_commits):
             "loc_sum": loc_sum,
             "day": commit_day,
         }
-        logger.info(str(result))
         yield result
 
 
-# TODO: Make this into a generator
-
-
 def git_diff_tree(hashX, hashY):
-    logger = logging.getLogger("LOC.git_diff_tree()")
     command = "git diff-tree -r %(hashX)s %(hashY)s" % vars()
     delta_loc = 0
     with os.popen(command) as inf:
@@ -141,7 +126,6 @@ def git_diff_tree(hashX, hashY):
             elif operation == "M":
                 process_merge(info)
             else:
-                logger.error("Unknown operation '%s'" % operation)
                 continue
             delta_loc += info["loc"]
             yield info
