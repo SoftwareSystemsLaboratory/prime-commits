945eac8e2f26d423e9a0e26dcfb99e9126b3dd2f
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-07-19 21:03:02 +0000

    Add runner script

--- graph.py
+++ graph.py
@@ -22,3 +22,6 @@ def helloworld():
     fig = plt.figure()
     plt.plot([1, 2, 3, 4], [1, 4, 2, 3])
     fig.savefig("test.png")
+
+if __name__ == "__main__":
+    args: Namespace = get_argparse().parse_args()
