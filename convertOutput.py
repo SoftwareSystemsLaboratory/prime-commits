562745feb6b578a421e669b718d7f90ca2ba17c1
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-07-19 20:40:53 +0000

    Write runner script

--- convertOutput.py
+++ convertOutput.py
@@ -1,10 +1,7 @@
-import csv
-import json
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 
 import pandas
 from pandas import DataFrame
-from pandas.io.json import json_normalize
 
 
 def get_argparse() -> ArgumentParser:
@@ -22,16 +19,14 @@ def get_argparse() -> ArgumentParser:
     parser.add_argument(
         "--csv",
         help="Flag to set the output of the conversion to a .csv file",
-        default=None,
-        type=bool,
         required=False,
+        action="store_true",
     )
     parser.add_argument(
         "--tsv",
         help="Flag to set the output of the conversion to a .tsv file",
-        default=None,
-        type=bool,
         required=False,
+        action="store_true",
     )
     return parser
 
@@ -49,7 +44,19 @@ def convertToTSV(df: DataFrame, filename: str) -> None:
 
 
 if __name__ == "__main__":
-    args = get_argparse()
-    df: DataFrame = createDataframe("output.json")
-    convertToCSV(df, "output.csv")
-    convertToTSV(df, "output.tsv")
+    args: Namespace = get_argparse().parse_args()
+
+    if args.csv is None and args.tsv is None:
+        print('Run "python convertOutput -h" for a full list of arguements...')
+        quit(1)
+
+    filename: str = args.input
+    df: DataFrame = createDataframe(filename=filename)
+
+    filenamePrefix: str = filename.split(".json")[0]
+
+    if args.csv:
+        convertToCSV(df, filename=filenamePrefix + ".csv")
+
+    if args.tsv:
+        convertToTSV(df, filename=filenamePrefix + ".tsv")
