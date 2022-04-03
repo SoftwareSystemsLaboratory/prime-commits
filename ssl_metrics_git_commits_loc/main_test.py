1c6e443667fdfe95954a5403a876b47b4f4db237
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-04-03 16:37:43 -0500

    Create test for parseCommitLineFromLog

--- ssl_metrics_git_commits_loc/main_test.py
+++ ssl_metrics_git_commits_loc/main_test.py
@@ -1,3 +1,4 @@
+from dateutil.parser import parse as dateParse
 from argparse import Namespace
 import unittest
 import main
@@ -6,10 +7,19 @@ class TestMain(unittest.TestCase):
     def setUp(self) -> None:
         return super().setUp()
 
-    def test_repoExists(self) -> bool:
+    def test_repoExists(self) -> None:
         trueDirectory: str = "../"
         falseDirectory: str = "./"
         self.assertTrue(main.repoExists(directory=trueDirectory))
         self.assertFalse(main.repoExists(directory=falseDirectory))
         self.assertTrue(type(main.repoExists(directory=trueDirectory)) == bool)
         self.assertTrue(type(main.repoExists(directory=falseDirectory)) == bool)
+
+    def test_parseCommitLineFromLog(self)   ->  None:
+        name: str = "Wild E. Coyote"
+        email: str = "wec@mail.com"
+        hash: str = "123abc"
+        date: str = "1-2-90"    # Month - Day - Year
+        parameter: str = ";".join([name, email, hash, date])
+        testCase: dict = {"author_name": name, "author_email": email, "hash": hash, "date": dateParse(date)}
+        self.assertTrue(main.parseCommitLineFromLog(line=parameter) == testCase)
