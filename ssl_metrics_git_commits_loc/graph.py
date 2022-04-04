2429de20df5cb8218974cbeb0a1274b46815fd13
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-04-03 22:18:52 -0500

    Add styling to the plots

--- ssl_metrics_git_commits_loc/graph.py
+++ ssl_metrics_git_commits_loc/graph.py
@@ -1,3 +1,4 @@
+from signal import pause
 import matplotlib.pyplot as plt
 import pandas
 from pandas import DataFrame
@@ -8,7 +9,6 @@ def computeXY(
     yKey: str,
     xKey: str = "author_days_since_0",
     yThousandth: bool = False,
-    xStepper: int = 1,
 ) -> tuple:
     xData: set = df[xKey].unique().tolist()
     yData: list = []
@@ -19,7 +19,11 @@ def computeXY(
             yData.append(df.loc[df[xKey] == day, yKey].sum() / 1000)
     else:
         for day in xData:
-            yData.append(df.loc[df[xKey] == day, yKey].sum() / 1000)
+            print(df.loc[df[xKey] == day, yKey])
+            input()
+
+
+            yData.append(df.loc[df[xKey] == day, yKey].sum())
     return (xData, yData)
 
 
@@ -27,12 +31,15 @@ def plot(
     x: list,
     y: list,
     type: str = "line",
-    xAxis: str = "",
-    yAxis: str = "",
+    xLabel: str = "",
+    yLabel: str = "",
     title: str = "",
     output: str = "commits_loc.pdf",
+    stylesheet: str = "style.mplstyle"
 ) -> None:
     "param: type can only be one of the following: line, bar"
+
+    plt.style.use(stylesheet)
 
     if type == "line":
         plt.plot(x, y)
@@ -40,14 +47,19 @@ def plot(
         plt.bar(x, height=y)
     else:
         print(f"Invalid plot type: {type}")
-    plt.savefig(output)
+
+    plt.title(title)
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
+
+    plt.savefig(output, bbox_inches='tight', transparent="True", pad_inches=0)
 
 
 def main() -> None:
-    df: DataFrame = pandas.read_json("commit_loc.json")
+    df: DataFrame = pandas.read_json("commits_loc.json")
     df = df.T
-    data: tuple = computeXY(df=df, yKey="added_lines_of_code", yThousandth=True)
-    plot(x=data[0], y=data[1], type="bar")
+    data: tuple = computeXY(df=df, yKey="added_lines_of_code", yThousandth=False)
+    plot(x=data[0], y=data[1], type="bar", title="TEST")
 
 
 if __name__ == "__main__":
