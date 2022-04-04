b0f0697f793966cd44ef5bcd9308869c20c57600
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-04-03 21:59:38 -0500

    Ran black and isort

--- ssl_metrics_git_commits_loc/graph.py
+++ ssl_metrics_git_commits_loc/graph.py
@@ -1,24 +1,45 @@
+import matplotlib.pyplot as plt
 import pandas
 from pandas import DataFrame
-import matplotlib.pyplot as plt
 
-def computeXY(df: DataFrame, yKey:str, xKey: str = "author_days_since_0", xStepper: int = 1)   ->  tuple:
+
+def computeXY(
+    df: DataFrame, yKey: str, xKey: str = "author_days_since_0", xStepper: int = 1
+) -> tuple:
     xData: set = df[xKey].unique().tolist()
     yData: list = []
     day: int
     for day in xData:
         yData.append(df.loc[df[xKey] == day, yKey].sum())
-    return((xData, yData))
+    return (xData, yData)
 
-def plot(x: list, y: list, xAxis: str = "", yAxis: str = "", title: str = "", output: str = "commits_loc.pdf") ->  None:
-    plt.plot(x, y)
+
+def plot(
+    x: list,
+    y: list,
+    type: str = "line",
+    xAxis: str = "",
+    yAxis: str = "",
+    title: str = "",
+    output: str = "commits_loc.pdf",
+) -> None:
+    "type can only be one of the following: line, bar"
+
+    if type == "line":
+        plt.plot(x, y)
+    elif type == "bar":
+        plt.bar(x, height=y)
+    else:
+        print(f"Invalid plot type: {type}")
     plt.savefig(output)
 
-def main()  ->  None:
-    df: DataFrame = pandas.read_json('commit_loc.json')
+
+def main() -> None:
+    df: DataFrame = pandas.read_json("commit_loc.json")
     df = df.T
     data: tuple = computeXY(df=df, yKey="added_lines_of_code")
-    plot(x=data[0], y=data[1])
+    plot(x=data[0], y=data[1], type="bar")
+
 
 if __name__ == "__main__":
     main()
