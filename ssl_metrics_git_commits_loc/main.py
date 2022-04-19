8c9755e825e2275ab15bd4aba174dab9de241dcd
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-04-19 12:25:33 -0500

    Flesh out the logging of the application

--- ssl_metrics_git_commits_loc/main.py
+++ ssl_metrics_git_commits_loc/main.py
@@ -6,18 +6,25 @@ from datetime import datetime
 from os.path import exists, join
 
 from dateutil.parser import parse as dateParse
-from pandas import DataFrame
+from pandas import DataFrame, Series
 from progress.bar import Bar
 
 from ssl_metrics_git_commits_loc.args import mainArgs
 
 def repoExists(directory: str = ".") -> bool:
-    return exists(join(directory, ".git"))
+    isGitRepository: bool = exists(join(directory, ".git"))
+    if isGitRepository:
+        logging.debug(f"{directory} has a .git folder")
+    else:
+        logging.debug(f"{directory} doesn't have a .git folder")
+    return isGitRepository
 
 
 def gitCommits() -> list:
     with os.popen(r'git log --reverse --pretty=format:"%H"') as commits:
-        return [commit.strip() for commit in commits]
+        commitList: list = [commit.strip() for commit in commits]
+        logging.debug(f"List of commits from Git repository: {commitList}")
+        return commitList
 
 
 def commitMetadata(commit: str) -> list:
@@ -25,7 +32,9 @@ def commitMetadata(commit: str) -> list:
     with os.popen(
         rf'git log {commit} -1 --pretty=format:"%H;%an;%ae;%as;%at;%cn;%ce;%cs;%ct"'
     ) as info:
-        return info.read().split(";")
+        data: str = info.read()
+        logging.info(f"{commit} information:\n{data}")
+        return data.split(";")
 
 
 def commitLOC(commit: str, options: str = "", processes: int = 0) -> list:
@@ -38,8 +47,11 @@ def commitLOC(commit: str, options: str = "", processes: int = 0) -> list:
     with os.popen(command) as info:
         data: dict = json.load(info)
         df: DataFrame = DataFrame(data)
-        return df["SUM"].dropna().sort_index().to_list()
-        # Order of output [blanks, code, comments, nfiles]
+        output: Series = df["SUM"].dropna().sort_index()
+        logging.info(f"Commit {commit} cloc information:\n{output}")
+        logging.debug(f"Series to list conversion for commit {commit}: {output.to_list()}")
+        logging.info("Output should be in order: [blanks, code, comments, nfiles]")
+        return output.to_list()
 
 
 def commitsDiff(commit1: str, commit2: str, str="", processes: int = 0) -> list:
@@ -51,19 +63,30 @@ def commitsDiff(commit1: str, commit2: str, str="", processes: int = 0) -> list:
         try:
             data: dict = json.load(info)
         except json.JSONDecodeError as e:
-            print(
+            logging.warning(
                 f"\nERROR: Couldn't convert to JSON between commits {commit1} and {commit2}"
             )
             return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         df: DataFrame = DataFrame(data["SUM"])
-        output.extend(df["added"].dropna().sort_index().to_list())
-        output.extend(df["same"].dropna().sort_index().to_list())
-        output.extend(df["modified"].dropna().sort_index().to_list())
-        output.extend(df["removed"].dropna().sort_index().to_list())
-        return output
-        # Order of keys [added, same, modified, removed]
-        # Order of output [blanks, code, comments, nfiles]
 
+        added: Series = df["added"].dropna().sort_index()
+        same: Series = df["same"].dropna().sort_index()
+        modified: Series = df["modified"].dropna().sort_index()
+        removed: Series = df["removed"].dropna().sort_index()
+
+        logging.debug(f"Added diff between commits {commit1} and {commit2}:\n{added}")
+        logging.debug(f"Same diff between commits {commit1} and {commit2}:\n{same}")
+        logging.debug(f"Modified diff between commits {commit1} and {commit2}:\n{modified}")
+        logging.debug(f"Removed diff between commits {commit1} and {commit2}:\n{removed}")
+
+        output.extend(added.to_list())
+        output.extend(same.to_list())
+        output.extend(modified.to_list())
+        output.extend(removed.to_list())
+
+        logging.debug(f"Series to list conversion for the diff of commits {commit1} and {commit2}: {output}")
+        logging.info("Output should be in order: [blanks, code, comments, nfiles] in key order: [added, same, modified, removed]")
+        return output
 
 def commitsDelta(newLOC: list, oldLOC: list) -> list:
     return [a - b for a, b in zip(newLOC, oldLOC)]
@@ -74,6 +97,7 @@ def main() -> bool:
     args: Namespace = mainArgs()
 
     logging.basicConfig(level=logging.DEBUG, filename=args.log, filemode="a", format='%(process)d-%(asctime)s-%(levelname)s: %(message)s')
+    logging.info("Started logging...")
 
     if repoExists(directory=args.directory) is False:
         print(f"Invalid Git repository directory: {args.directory}")
@@ -81,6 +105,7 @@ def main() -> bool:
 
     os.chdir(args.directory)
     os.system(f"git checkout {args.branch} --quiet")
+    logging.info(f"Checked out Git branch {args.branch}")
 
     df: DataFrame = DataFrame(
         columns=[
@@ -123,11 +148,12 @@ def main() -> bool:
     )
 
     commits: list = gitCommits()
-
+    logging.info("Started iterating through commits\n")
     with Bar("Getting data from commits...", max=len(commits)) as bar:
         previousLOC: list = []
         c: int
         for c in range(len(commits)):
+            logging.info(f"On commit {c}: ({commits[c]})")
             data: list = commitMetadata(commit=commits[c])
             loc: list = commitLOC(
                 commits[c], options=args.cloc, processes=args.processes
@@ -142,6 +168,7 @@ def main() -> bool:
                 diff.extend([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
                 delta: list = loc
+                logging.info("Diff isn't calculated because there was no previous commit")
             else:
                 diff: list = commitsDiff(commit1=commits[c], commit2=commits[c - 1])
                 delta = commitsDelta(loc, previousLOC)
@@ -163,6 +190,7 @@ def main() -> bool:
             df.loc[len(df.index)] = data
 
             previousLOC = loc
+            logging.info(f"End of commit {c}: {commits[c]}\n")
 
             bar.next()
 
